73b406a400ae4dfd821523b1a78aaafc1e7ff314
Michael Goulding <Michael.Goulding@microsoft.com>
2022-11-09 14:43:01 -0800

    Add workaround

--- OptionalCrash.cpp
+++ OptionalCrash.cpp
@@ -19,6 +19,12 @@ private:
         std::optional<int> qubitId;
 
         auto operator<=>(const MaximizedViewId &) const = default;
+
+        // If you comment out the line above and uncomment out the below, it compiles fine.
+        //bool operator<(const MaximizedViewId & right) const
+        //{
+        //    return std::tie(type, qubitId) < std::tie(right.type, right.qubitId);
+        //}
     };
 
     void foo()
